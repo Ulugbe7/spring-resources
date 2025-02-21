26e4c3cbb59cafef4ce7d7240ceb4083d6f1bc67
Ulug'bek <71371994+Ulugbe7@users.noreply.github.com>
2025-02-21 17:41:01 +0500

    Add files via upload

---
+++ SwaggerConfiguration.java
@@ -0,0 +1,32 @@
+package uz.zaytun.agentms.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    private SecurityScheme createAPIKeySchema() {
+        return new SecurityScheme()
+                .type(HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeySchema()))
+                .info(new Info().title("Title")
+                        .description("REST APIs FOR Spring project")
+                        .version("0.0.1"));
+    }
+}
